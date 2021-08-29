71a63a618d13e29fa847dee45fe8daccddadadca
cgilly17 <89664912+CGilly17@users.noreply.github.com>
2021-08-29 21:47:30 +0000

    Added new line for my name.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-class Main {
-  public static void main(String[] args) {
+class Main 
+{
+  public static void main(String[] args) 
+  {
     System.out.println("Hello world!");
+    System.out.println("My name is CalLind Gilson.");
+    
   }
 }
